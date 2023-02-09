e7441c0835fbe9fd44f5d63fc8604fefa531b523
Kelly Zhou <yinglinzhou0@gmail.com>
2023-02-09 15:47:03 -0500

    done!

--- practice-for-ch-sql-associations-long-practice/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice/app/models/course.rb
@@ -19,5 +19,17 @@ class Course < ApplicationRecord
   has_many :enrolled_students,
     through: :enrollments,
     source: :student
+
+  belongs_to :prerequisite,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course,
+    optional: true
+  
+  belongs_to :instructor,
+  primary_key: :id,
+  foreign_key: :instructor_id,
+  class_name: :User
+
 end
 
