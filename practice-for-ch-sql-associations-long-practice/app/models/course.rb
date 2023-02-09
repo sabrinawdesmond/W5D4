96f1f2b7174026f36cf96018249c4d89423cb237
Kelly Zhou <yinglinzhou0@gmail.com>
2023-02-09 15:13:49 -0500

    enrolled

--- practice-for-ch-sql-associations-long-practice/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice/app/models/course.rb
@@ -14,4 +14,10 @@ class Course < ApplicationRecord
     primary_key: :id,
     foreign_key: :course_id,
     class_name: :Enrollment
+
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :student
 end
+
