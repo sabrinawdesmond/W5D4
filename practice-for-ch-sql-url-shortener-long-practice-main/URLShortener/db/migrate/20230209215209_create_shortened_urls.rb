acaba8c20d5d847de4ab9c4513b295509a5d2fe4
Sabrina Desmond <sabrinawdesmond@gmail.com>
2023-02-09 16:59:53 -0500

    short-urls

---
+++ practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/db/migrate/20230209215209_create_shortened_urls.rb
@@ -0,0 +1,12 @@
+class CreateShortenedUrls < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shortened_urls do |t|
+      t.string :short_url 
+      t.string :long_url
+
+      t.timestamps
+    end
+    add_index :shortened_urls, :long_url, unique: true
+    add_index :shortened_urls, :short_url
+  end
+end
